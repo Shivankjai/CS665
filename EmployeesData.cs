97aba8075c56e592e974a1a754e335ce9c851e5f
Shivank Jaiswal <93807239+Shivankjai@users.noreply.github.com>
2025-04-13 09:21:54 -0500

    Models
    Created class for Employees

---
+++ EmployeesData.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace MyDbApp.Models
+{
+    public class EmployeesData
+    {
+        public int Id { get; set; }
+        
+        public int manager_id { get; set; }
+
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+
+        public int branch_id { get; set; }
+
+        public int salary { get; set; }
+
+    }
+}
