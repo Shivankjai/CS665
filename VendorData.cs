c06000d46c9cd360288addd079e20b6127fcc76e
Shivank Jaiswal <93807239+Shivankjai@users.noreply.github.com>
2025-04-13 09:06:53 -0500

    Models
    Created a Vendor Model for using the Post and Put in the Controller file

---
+++ VendorData.cs
@@ -0,0 +1,9 @@
+﻿namespace MyDbApp.Models
+{
+    public class VendorData
+    {
+        public int Id { get; set; }
+       public string vendor_name { get; set; }
+        public string vendor_product_type { get; set; }
+    }
+}
