65a420f1835ef268799831fe035e10db21b673e3
Shivank Jaiswal <93807239+Shivankjai@users.noreply.github.com>
2025-04-13 09:26:27 -0500

    Models
    Created classes

---
+++ ClientsData.cs
@@ -0,0 +1,13 @@
+﻿namespace MyDbApp.Models
+{
+    public class ClientsData
+    {
+        public int Id { get; set; }
+
+        public string company_name { get; set; }
+
+        public string client_email_id { get; set; }
+
+        public int employee_id { get; set; }
+    }
+}
---
+++ VendorRelationData.cs
@@ -0,0 +1,9 @@
+﻿namespace MyDbApp.Models
+{
+    public class VendorRelationData
+    {
+        public int branch_id { get; set; }
+
+        public int vendor_id { get; set; }
+    }
+}
