062aadb1502c31b315c25c14ef5279e0c7f8f781
Shivank Jaiswal <93807239+Shivankjai@users.noreply.github.com>
2025-04-13 09:12:11 -0500

    Models
    Created class for OfficeBranchesData

---
+++ OfficeBranchesData.cs
@@ -0,0 +1,12 @@
+﻿//ing System.Globalization;
+
+namespace MyDbApp.Models
+{
+    public class OfficeBranchesData
+    {
+        public int Id { get; set; }
+        public string country_name { get; set; }
+        public String branch_founder_name { get; set; }
+        public string branch_name { get; set; }
+    }
+}
