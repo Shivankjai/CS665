c011c522e0460c77a0632044d9ce1f948b3e0e5f
Shivank Jaiswal <93807239+Shivankjai@users.noreply.github.com>
2025-04-13 09:01:53 -0500

    DbContext
    Created a DbContext file

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,52 @@
+﻿//using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+using System.Data.Common;
+
+namespace MyDbApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public async Task<List<Dictionary<string, object>>> ExecuteSqlQueryAsync(string sql, params DbParameter[] parameters)
+        {
+            var result = new List<Dictionary<string, object>>();
+
+            using (var command = Database.GetDbConnection().CreateCommand())
+            {
+                command.CommandText = sql;
+                command.CommandType = CommandType.Text;
+
+                if (parameters != null && parameters.Length > 0)
+                {
+                    foreach (var parameter in parameters)
+                    {
+                        command.Parameters.Add(parameter);
+                    }
+                }
+
+                await Database.OpenConnectionAsync();
+
+                using (var reader = await command.ExecuteReaderAsync())
+                {
+                    while (await reader.ReadAsync())
+                    {
+                        var row = new Dictionary<string, object>();
+
+                        for (var i = 0; i < reader.FieldCount; i++)
+                        {
+                            row[reader.GetName(i)] = reader.IsDBNull(i) ? null : reader.GetValue(i);
+                        }
+
+                        result.Add(row);
+                    }
+                }
+
+                await Database.CloseConnectionAsync();
+            }
+
+            return result;
+        }
+    }
+}
