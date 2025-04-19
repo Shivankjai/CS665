b41b3fcd63efa1d671c15b21a544d01f1aa1b307
Shivank Jaiswal <93807239+Shivankjai@users.noreply.github.com>
2025-04-19 10:51:30 -0500

    Changed the name of OfficeBranch Class to match
    with Controller file

--- OfficeBranchesData.cs
+++ OfficeBranchesData.cs
@@ -4,7 +4,7 @@ namespace MyDbApp.Models
 {
     public class OfficeBranchesData
     {
-        public int Id { get; set; }
+        public int branch_id { get; set; }
         public string country_name { get; set; }
         public String branch_founder_name { get; set; }
         public string branch_name { get; set; }
